caac5db4032b55079e38a84ce1a39ea320abec9e
maskedmarker <changjianghui0822@gmail.com>
2023-12-10 14:50:00 +0800

    暂存

---
+++ hello-world/src/main/java/org/example/learn/spring/batch/App.java
@@ -0,0 +1,13 @@
+package org.example.learn.spring.batch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
